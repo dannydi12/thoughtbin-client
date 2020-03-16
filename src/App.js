8c61e0df52f817c9366a8d44fdad332fc6d7aebb
Daniel Di Venere <danieldivenere@gmail.com>
2020-03-16 15:03:22 -0700

    button change achieved for thought display

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ThoughtList from './ThoughtList/ThoughtList';
+import { useHistory, useLocation } from "react-router-dom";
 import './App.css';
 
 function App() {
+  const history = useHistory();
+  const location = useLocation();
+
+  const handleClick = path => {
+    history.push(path)
+  }
+
   return (
     <main>
       <header className='banner'>
@@ -12,10 +20,15 @@ function App() {
       <section>
         <h3>What's on your mind?</h3>
         <div>
-          <button>Make a thought</button>
-          <button>Make a thought</button>
+          <button onClick={() => handleClick('/thoughts/create')}>Make a thought</button>
+          {
+            location.pathname === '/' ?
+              <button onClick={() => handleClick('/thoughts')}>My Thoughts</button>
+              : <button onClick={() => handleClick('/')}>Others' Thoughts</button>
+          }
         </div>
       </section>
+      <ThoughtList />
     </main>
   );
 }
