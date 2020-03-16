3b91a7a75fa110fe3d029341f19650a87e98ee5b
Daniel Di Venere <danieldivenere@gmail.com>
2020-03-16 16:20:12 -0700

    added edit mock functionality

--- src/ThoughtCard/ThoughtCard.js
+++ src/ThoughtCard/ThoughtCard.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ThoughtForm from '../ThoughtForm/ThoughtForm'
 import './ThoughtCard.css';
 
 function ThoughtCard(props) {
+  const [isEditing, setIsEditing] = useState(false)
   return (
     <div>
-      <p>{props.content}</p>
+      {isEditing ? <ThoughtForm content={props.content} /> : <p>{props.content}</p>}
+      <div>
+        <button>Share</button>
+        <button onClick={() => setIsEditing(true)}>Edit icon</button>
+        <button>Trash icon</button>
+      </div>
     </div>
   )
 }
