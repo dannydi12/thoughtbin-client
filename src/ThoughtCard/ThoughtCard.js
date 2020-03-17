c9eabf88403fb9316e695fbe6eb68ee38b9236b1
Daniel Di Venere <danieldivenere@gmail.com>
2020-03-17 10:41:40 -0700

    styled and positioned buttons

--- src/ThoughtCard/ThoughtCard.js
+++ src/ThoughtCard/ThoughtCard.js
@@ -1,24 +1,31 @@
 import React, { useState } from 'react';
 import { useRouteMatch } from "react-router-dom";
-import ThoughtForm from '../ThoughtForm/ThoughtForm'
+import ThoughtForm from '../ThoughtForm/ThoughtForm';
+import ShareButton from '../buttons/ShareButton/ShareButton';
+import EditButton from '../buttons/EditButton/EditButton';
 import './ThoughtCard.css';
+import DeleteButton from '../buttons/DeleteButton/DeleteButton';
 
 function ThoughtCard(props) {
   const [isEditing, setIsEditing] = useState(false)
   const match = useRouteMatch('/thoughts')
+
+  const copyLink = () => {
+    console.log('copied')
+  }
+
+  const deleteThought = () => {
+    console.log('deleted')
+  }
+
   return (
     <div className='thought-card'>
       {isEditing ? <ThoughtForm content={props.content} /> : <p>{props.content}</p>}
       <div className='button-wrapper'>
-        <button>Share</button>
+        {match && <EditButton clickFunction={() => setIsEditing(true)} />}
+        {match && <DeleteButton clickFunction={deleteThought}/>}
+        <ShareButton clickFunction={copyLink} />
 
-        {
-          match &&
-          <>
-            <button onClick={() => setIsEditing(true)}>Edit icon</button>
-            <button>Trash icon</button>
-          </>
-        }
       </div>
     </div>
   )
