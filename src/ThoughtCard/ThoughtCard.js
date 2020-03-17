3f571af08ceb8e99ced5e387f0c08a9c8f297363
Daniel Di Venere <danieldivenere@gmail.com>
2020-03-16 17:57:35 -0700

    conditional rendering applied for buttons

--- src/ThoughtCard/ThoughtCard.js
+++ src/ThoughtCard/ThoughtCard.js
@@ -1,16 +1,22 @@
 import React, { useState } from 'react';
+import { useRouteMatch } from "react-router-dom";
 import ThoughtForm from '../ThoughtForm/ThoughtForm'
 import './ThoughtCard.css';
 
 function ThoughtCard(props) {
   const [isEditing, setIsEditing] = useState(false)
+  const match = useRouteMatch('/thoughts')
   return (
     <div>
       {isEditing ? <ThoughtForm content={props.content} /> : <p>{props.content}</p>}
       <div>
         <button>Share</button>
-        <button onClick={() => setIsEditing(true)}>Edit icon</button>
-        <button>Trash icon</button>
+
+        {match && <>
+          <button onClick={() => setIsEditing(true)}>Edit icon</button>
+          <button>Trash icon</button>
+        </>
+        }
       </div>
     </div>
   )
