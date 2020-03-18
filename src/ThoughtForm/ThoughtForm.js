dd51b8144bbc467a5aa1d4f2a64584c766429ddb
Daniel Di Venere <danieldivenere@gmail.com>
2020-03-17 20:46:56 -0700

    started styles on new thought form

--- src/ThoughtForm/ThoughtForm.js
+++ src/ThoughtForm/ThoughtForm.js
@@ -3,9 +3,9 @@ import './ThoughtForm.css';
 
 function ThoughtForm(props) {
   return (
-    <form>
+    <form className='new-thought-form'>
       {/* dont want a label but must still be a11y */}
-      <textarea placeholder='An essay on why bananas are green...' defaultValue={props.content}></textarea>
+      <textarea placeholder='An essay on why bananas are green...' defaultValue={props.content} />
       <button type='submit'>Express</button>
     </form>
   )
--- src/ThoughtList/ThoughtList.js
+++ src/ThoughtList/ThoughtList.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import { Route } from "react-router-dom";
+import { useRouteMatch } from "react-router-dom";
 import ThoughtCard from '../ThoughtCard/ThoughtCard';
 import ThoughtForm from '../ThoughtForm/ThoughtForm';
 import thoughts from './thought-fixture';
 import './ThoughtList.css';
 
 function ThoughtList() {
+  const match = useRouteMatch('/thoughts')
   const allThoughts = thoughts.map(thought => <ThoughtCard key={thought.id} id={thought.id} content={thought.content} />)
   //TO-DO: if on /thoughts, filter array for thoughts with matching user id
 
   return (
     <section className='thought-list'>
-      <Route path='/thoughts'>
-        <ThoughtForm />
-      </Route>
+      {match && <ThoughtForm />}
       {allThoughts}
     </section>
   )
