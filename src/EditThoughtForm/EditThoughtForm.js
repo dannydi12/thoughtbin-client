6a6903c31288a3c7d3f98dfc9e35a91405b4c4a4
Daniel Di Venere <danieldivenere@gmail.com>
2020-03-18 13:24:23 -0700

    automatically selects text when editing a thought

--- src/EditThoughtForm/EditThoughtForm.js
+++ src/EditThoughtForm/EditThoughtForm.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './EditThoughtForm.css';
 
 function EditThoughtForm(props) {
+  useEffect(() => {
+    const thoughtElement = document.getElementById(`thought-${props.id}`).elements.thought;
+    thoughtElement.focus();
+    thoughtElement.selectionStart = thoughtElement.value.length
+  });
+
   return (
-    <form className='thought-form'>
-      {/* dont want a label but must still be a11y */}
-      <textarea aria-label="Edit your thought" defaultValue={props.content}></textarea>
+    <form id={`thought-${props.id}`} className='thought-form'>
+      <textarea name='thought' aria-label="Edit your thought" defaultValue={props.content}></textarea>
       <div className='edit-thought-button-wrapper'>
         <button onClick={props.cancelEdit} type='button'>Cancel</button>
         <button onClick={props.editThought} type='submit'>Re-Express</button>
