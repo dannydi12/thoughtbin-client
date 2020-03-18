aa94eb655a9baff4b301435f8581f4031be403fe
Daniel Di Venere <danieldivenere@gmail.com>
2020-03-18 12:53:35 -0700

    fixed poory named css selectors in buttons

--- src/buttons/EditButton/EditButton.js
+++ src/buttons/EditButton/EditButton.js
@@ -3,7 +3,7 @@ import './EditButton.css';
 
 function EditButton(props) {
   return (
-    <button aria-label='edit' className='delete-button' onClick={props.clickFunction}>
+    <button aria-label='edit' className='edit-button' onClick={props.clickFunction}>
       <i aria-hidden='true'>
         <svg viewBox="0 -1 401.52289 401" xmlns="http://www.w3.org/2000/svg">
           <path d="m370.589844 250.972656c-5.523438 0-10 4.476563-10 10v88.789063c-.019532 16.5625-13.4375 29.984375-30 30h-280.589844c-16.5625-.015625-29.980469-13.4375-30-30v-260.589844c.019531-16.558594 13.4375-29.980469 30-30h88.789062c5.523438 0 10-4.476563 10-10 0-5.519531-4.476562-10-10-10h-88.789062c-27.601562.03125-49.96875 22.398437-50 50v260.59375c.03125 27.601563 22.398438 49.96875 50 50h280.589844c27.601562-.03125 49.96875-22.398437 50-50v-88.792969c0-5.523437-4.476563-10-10-10zm0 0" />
