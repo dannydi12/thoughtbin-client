3d9f6bdebe32af33f2071155b3f3e34adea1d14a
Daniel Di Venere <danieldivenere@gmail.com>
2020-03-18 17:48:30 -0700

    textarea autoexpands

--- src/NewThoughtForm/NewThoughtForm.js
+++ src/NewThoughtForm/NewThoughtForm.js
@@ -2,10 +2,18 @@ import React from 'react';
 import './NewThoughtForm.css';
 
 function NewThoughtForm(props) {
+  const autoExpand = (field) => {
+    field.style.height = 'inherit';
+    field.style.height = field.scrollHeight + 'px';
+  };
+
   return (
     <form className='thought-form'>
-      {/* dont want a label but must still be a11y */}
-      <textarea id='create-new-thought' aria-label="Post a thought" placeholder='An essay on why bananas are green...' defaultValue={props.content} />
+      <textarea id='create-new-thought'
+        aria-label="Post a thought"
+        placeholder='An essay on why bananas are green...'
+        defaultValue={props.content}
+        onChange={(e) => autoExpand(e.target)} /> {/* Should call a handler that keeps character count, autoexpands, etc */}
       <button type='submit'>Express</button>
     </form>
   )
