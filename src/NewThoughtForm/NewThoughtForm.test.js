fe28556014310784135d8603f28bbc84204a4061
Daniel Di Venere <danieldivenere@gmail.com>
2020-04-04 10:28:35 -0700

    fixed smoke test

--- src/NewThoughtForm/NewThoughtForm.test.js
+++ src/NewThoughtForm/NewThoughtForm.test.js
@@ -3,7 +3,14 @@ import ReactDOM from 'react-dom';
 import NewThoughtForm from './NewThoughtForm';
 
 it('renders without crashing', () => {
+  const thought = {
+    id: 1,
+    userId: 'any-user-id',
+    created_at: new Date('2039-01-22T16:28:32.615Z').toISOString(),
+    content: 'We do not merely destroy our enemies; we change them.',
+  };
+
   const div = document.createElement('div');
-  ReactDOM.render(<NewThoughtForm />, div);
+  ReactDOM.render(<NewThoughtForm thought={thought} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
